f33da6c74e207ab2a061c2a231fad05bae312a78
faustologic <69010388+faustologic@users.noreply.github.com>
2020-09-09 10:02:53 -0500

    Add Checkout  page
    Add the checkout page as child of the home page, also we update the header component as one in the App.js file.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import Header from "./Header"
 import Home from "./Home"
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Checkout from "./Checkout"
 
 function App() {
   return (
     // BEM Convention
     <Router>
     <div className="app">
+    <Header />
+    
       <Switch>
         <Route path="/checkout">
-          <h1>I'm a checkout page</h1>
+        <Checkout />
         </Route>
         <Route path="/">
-          <Header />
           <Home />
         </Route>
         </Switch>      
