dc9bc897905dc199b5676170da496f18bbdaa22f
faustologic <69010388+faustologic@users.noreply.github.com>
2020-09-08 19:52:39 -0500

    Amazon-clone update
    I'm building an amazon-clone app e-commerce web, using reactJS and a firebase databe server.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA5nHAiSLyk6TLQR1I0xvsq6z7EuP5hKEA",
+    authDomain: "challenge-cb568.firebaseapp.com",
+    databaseURL: "https://challenge-cb568.firebaseio.com",
+    projectId: "challenge-cb568",
+    storageBucket: "challenge-cb568.appspot.com",
+    messagingSenderId: "1025374821744",
+    appId: "1:1025374821744:web:b31c0e6e06ba46d2d883b5",
+    measurementId: "G-LR0019G41M"
+  };
