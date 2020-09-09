9bdbb216289034d76c853fd38923a5c71435e58b
faustologic <69010388+faustologic@users.noreply.github.com>
2020-09-09 00:46:53 -0500

    Upadate files
    Install react-router-dom
    Home.js
    Product.js with properties props useful
    Put some info on the Home Page
    And declare the /checkout page for the checkout products.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from "./Header"
 import Home from "./Home"
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
     // BEM Convention
+    <Router>
     <div className="app">
-      <Header />
-      <Home />
+      <Switch>
+        <Route path="/checkout">
+          <h1>I'm a checkout page</h1>
+        </Route>
+        <Route path="/">
+          <Header />
+          <Home />
+        </Route>
+        </Switch>      
     </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -11,16 +11,46 @@ function Home() {
                     src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220.jpg"
                 />
                 <div className="home__row">
-                    <Product/>
-                    {/* Product */}
+                    <Product 
+                    tittle="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
+                    image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+                    price={29.99}
+                    rating={3}
+                    />
+                    <Product 
+                    tittle="8Bitdo Sn30 Pro+ Bluetooth Gamepad (Sn Edition) - Nintendo Switch"
+                    image="https://images-na.ssl-images-amazon.com/images/I/41aV6wQdNgL._AC_SY200_.jpg"
+                    price={49.99}
+                    rating={5}
+                    />
                 </div>
                 <div className="home__row">
-                    {/* Product */}
-                    {/* Product */}
-                    {/* Product */}
+                <Product 
+                    tittle="Super Mario 3D All-Stars - Nintendo Switch"
+                    image="https://m.media-amazon.com/images/I/51Hq9iXWvSL._SS135_.jpg"
+                    price={59.99}
+                    rating={5}
+                    />
+                    <Product 
+                    tittle="Bose SoundLink Micro, Portable Outdoor Speaker, (Wireless Bluetooth Connectivity), Bright Orange"
+                    image="https://m.media-amazon.com/images/I/41LhnhBTfXL._SS135_.jpg"
+                    price={29.99}
+                    rating={5}
+                    />
+                    <Product 
+                    tittle="Seiko Men's SNK809K Automatic Stainless Steel Watch"
+                    image="https://images-na.ssl-images-amazon.com/images/I/41x1Ukt0XnL._AC_SY200_.jpg"
+                    price={29.99}
+                    rating={3}
+                    />
                 </div>
                 <div className="home__row">
-                    {/* Product */}
+                <Product 
+                    tittle="Alienware M15-15.6 FHD Gaming Laptop Thin and Light, i7-8750H Processor, NVIDIA GeForce Graphics Card, 16GB RAM, 1TB Hybrid HDD + 128GB SSD, 17.9mm Thick & 4.78lbs"
+                    image="https://images-na.ssl-images-amazon.com/images/I/510a96rEUEL._AC_SY200_.jpg"
+                    price={1349.99}
+                    rating={4}
+                    />
                 </div>
 
             </div>
