3f27952e9f37735b9d968a0faf600eacc19c0220
faustologic <69010388+faustologic@users.noreply.github.com>
2020-10-22 18:34:35 -0500

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,67 +1,85 @@
-import React, { useState } from 'react'
-import "./Login.css"
-import { Link, useHistory } from 'react-router-dom'
-import { auth } from './firebase';
+import React, { useState } from "react";
+import "./Login.css";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
 
 function Login() {
-    const history = useHistory()
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const signIn = e => {
-        e.preventDefault()
+  const signIn = (e) => {
+    e.preventDefault();
 
-        auth.signInWithEmailAndPassword(email, password).then(auth => {
-            history.push("/")
-        })
-        .catch(error => alert(error.message));
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
 
-        // Firebase login 
-    }
+    // Firebase login
+  };
 
-    const register = e => {
-        e.preventDefault()
+  const register = (e) => {
+    e.preventDefault();
 
-        auth.createUserWithEmailAndPassword(email, password)
-            .then((auth) => {
-                // it succesfully create a new user with email and password
-                //console.log(auth);
-                if (auth) {
-                    history.push("/")
-                }
-            })
-        .catch(error => alert(error.message))
-        // Firebase register
-    }
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // it succesfully create a new user with email and password
+        //console.log(auth);
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+    // Firebase register
+  };
 
-    return (
-        
-        <div className="login">
-            <Link to="/">
-                <img 
-                    className="login__logo" 
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
-                />
-            </Link>
-            <div className="login__container">
-                <h1>Sign-In</h1>
-                <form>
-                    <h5>E-mail</h5>
-                    <input type="email" value={email} onChange={e => setEmail(e.target.value)}
-                    />
-                    <h5>Password</h5>
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}
-                    />
-                    <button type="submit" onClick={signIn} className="login__signInButton">Sign In</button>
-                </form>
-                <p>
-                    By signing-in you agree to the <strong>AMAZON FAKE CLONE</strong> Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and your Interest-Based Ads Notice.
-                </p>
-                <button onClick={register}
-                    className="login__registerButton">Create your Amazon Account</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-In</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to the <strong>AMAZON FAKE CLONE</strong>{" "}
+          Conditions of Use & Sale. Please see our Privacy Notice, our Cookies
+          Notice and your Interest-Based Ads Notice.
+        </p>
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
